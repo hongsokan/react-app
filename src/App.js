434a3187db3cb4527ad2a4c207104f93fc10e16c
hongsokan_mac <hongsokan@daum.net>
2020-01-16 14:13:22 +0900

    App.js 에서 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -2,11 +2,51 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
+class Subject extends Component {
+  render() {
+    return (
+      // 하나의 최상위 태그만 써야한다
+      // JSX 문법, html 태그 인식
+      <header>
+        <h1>WEB</h1>
+        World Wide Web!
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h>Hello, React!!</h>
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
       // <div className="App">
       //   <header className="App-header">
